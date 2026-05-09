aede8ec086d906f7ff1dc11008429dfc13752dc6
IrfanDarwish <epunkd14@gmail.com>
2026-05-09 10:14:51 +0800

    exercise 1: create basic pages and routes

---
+++ instructor-frontend/src/pages/InstructorDetailPage.jsx
@@ -0,0 +1,10 @@
+function InstructorDetailPage() {
+    return (
+        <div>
+        <h1>Instructor Detail</h1>
+        <p>This is the page for viewing instructor details.</p>
+        </div>
+    );
+}
+
+export default InstructorDetailPage;
