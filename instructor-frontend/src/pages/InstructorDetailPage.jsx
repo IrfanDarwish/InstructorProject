04509c8d5514bc8940b7e58069e1e67912c60436
IrfanDarwish <epunkd14@gmail.com>
2026-05-09 12:17:49 +0800

    exercise 3: use dynamic rouute parametes

--- instructor-frontend/src/pages/InstructorDetailPage.jsx
+++ instructor-frontend/src/pages/InstructorDetailPage.jsx
@@ -1,9 +1,14 @@
+import { useParams } from 'react-router-dom';
+
 function InstructorDetailPage() {
+    const { id } = useParams();
+
     return (
-        <div>
-        <h1>Instructor Detail</h1>
-        <p>This is the page for viewing instructor details.</p>
-        </div>
+        <section>
+            <h1>Instructor Detail</h1>
+            <p>Instructor ID: {id}</p>
+            <p>This is the page for viewing instructor details.</p>
+        </section>
     );
 }
 
