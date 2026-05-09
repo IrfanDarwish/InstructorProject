ef95c98987b7f10ddcd8e44a11db4076fa80be2b
IrfanDarwish <epunkd14@gmail.com>
2026-05-09 16:29:10 +0800

    exercise 6: implement backend login

---
+++ instructor-frontend/src/services/authAPI.js
@@ -0,0 +1,17 @@
+const AUTH_API_URL = 'http://localhost:8080/api/auth';
+
+export const loginUser = async (email, password) => {
+    const response = await fetch(`${AUTH_API_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            email: email, 
+            password: password })
+    });
+    if (!response.ok) {
+        throw new Error(`Failed to login user: ${response.statusText}`);
+    }
+    return await response.json();
+}
