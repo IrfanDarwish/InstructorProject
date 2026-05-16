a5cc4f82bef82f6c6f584d98f47381d4f6ab0ad9
IrfanDarwish <epunkd14@gmail.com>
2026-05-16 21:55:18 +0800

    handle cors issue

--- src/main/java/com/example/instructorapi/controller/AuthController.java
+++ src/main/java/com/example/instructorapi/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@CrossOrigin(origins = "http://localhost:5174") // Allow requests from the React frontend
+@CrossOrigin(origins = "http://localhost:5173") // Allow requests from the React frontend
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
